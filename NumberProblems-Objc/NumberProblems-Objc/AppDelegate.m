812625577d160f2074d5a89068b9a4f20100dde1
Brandon Beecroft <brandonbeecroft@gmail.com>
2014-08-28 16:36:52 -0600

    finish project 5 all steps

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -13,14 +13,46 @@
 @end
 
 @implementation AppDelegate
-            
+
+int multiple = 4;
+float divisor = 6.0;
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
-    
-    
+    [self iterateCount:4];
+
     return YES;
 }
+
+//step 2
+-(void)iterateCount:(int)aValue {
+
+    //step 3
+    int multiplyValue = [self multipled:aValue];
+    NSLog(@"Multipled %i by %i to be %i", aValue, multiple, multiplyValue);
+
+    float divideValue = [self divided:multiplyValue];
+    NSLog(@"Divided %i by %f to be %f", multiplyValue, divisor, divideValue);
+
+    while (aValue >= 0) {
+        NSLog(@"Iterate %i", aValue);
+        aValue--;
+    }
+}
+
+//step 3
+-(int)multipled:(int)aValue {
+    int newIntegerValue = aValue * multiple;
+    return newIntegerValue;
+}
+
+-(float)divided:(int)aValue {
+    float dividedValue = aValue / divisor;
+    return dividedValue;
+}
+
+// step 4
+// converted int's to floating point numbers
 
 @end
